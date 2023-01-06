59e73ae632d12b60bb7248468990af4ef9fe8b57
Olvin <paradox_x94@hotmail.com>
2023-01-06 16:36:34 +0300

    - Another TODO list update, fixed naming on scene

--- Assets/Scripts/ToDoList.cs
+++ Assets/Scripts/ToDoList.cs
@@ -9,13 +9,25 @@
     // 30) Hide UnitView in Unit
     // 31) Foot IK for unit
     // 32) Bootstrap scene
+    // 35) Water and swimming
     
     //TODO Models and animations:
     // 8) Unit sword fight with blocking
     // 28) Campfire model and sit around animation
+    // 37) Swimming animation
     
     //TODO Graphics, effects and UI:
     // 33) URP and Terrain shader (Done!)
     // 22) Simple UI with unit icons
     // 21) Damage labels
     // 34) Fire effect for campfire
+    // 36) Water effect
+
+    //Goals v0.1:
+    // - UI, show player units
+    // - Map with enemy and player units marks
+    // - Level loading, main menu
+    // - Effects, blood
+    // - Animations, attack animations
+    // - Simple sounds
+    // - Environment: trees, grass, water
